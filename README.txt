f70e5476f10ffad0f7eff6062a7a68f79ca1415f
watucker <william.tucker@stfc.ac.uk>
2019-11-19 12:14:38 +0000

    Initial commit from svn.

---
+++ README.txt
@@ -0,0 +1,3 @@
+jasmintheme_bootstrap3_standalone
+
+Web template for JASMIN Django apps based on bootstrap 3+.
---
+++ setup.py
@@ -0,0 +1,42 @@
+import os, re
+from setuptools import setup
+
+base_name='jasmintheme_bootstrap3_standalone'
+
+v_file = open(os.path.join(os.path.dirname(__file__), 
+                       base_name, '__init__.py'))
+VERSION = re.compile(r".*__version__ = '(.*?)'",
+                     re.S).match(v_file.read()).group(1)
+                     
+README = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name = base_name,
+    version = VERSION,
+    packages = [base_name,],
+    include_package_data = True,
+    license = 'BSD License', # example license
+    description = 'JASMIN django app templates',
+    long_description = README,
+    url = 'http://team.ceda.ac.uk/svn/ceda/ceda_software/jasminsite/themes/jasmintheme_bootstrap3_standalone',
+    author = 'Matt Pritchard',
+    author_email = 'matt.pritchard@stfc.ac.uk',
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'BSD licence, see LICENCE',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+      
+    # Adds dependencies    
+    #install_requires = [],
+)
